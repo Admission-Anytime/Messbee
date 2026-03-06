1f42ee62050a5378447689aaa24beef7c31c24c3
Arshlan Khan <152981104+arshlankhann@users.noreply.github.com>
2026-03-06 11:33:32 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -43,9 +43,6 @@ const corsOptions = {
 
     const allowedOrigins = [
       'http://localhost:5173',
-      'http://127.0.0.1:5173',
-      'http://localhost:5174',
-      'http://127.0.0.1:5174',
       process.env.CLIENT_URL
     ].filter(Boolean);
 
