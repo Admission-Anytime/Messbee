82fa7ca8f1070109f15409db83a5cdbc4d4344f5
Payal-creator <payalraw546@gnmai.com>
2026-04-23 11:20:11 +0530

    rectified dark mode of campaign launch page

--- client/src/pages/campaign/CampaignLaunchSuccess.jsx
+++ client/src/pages/campaign/CampaignLaunchSuccess.jsx
@@ -35,7 +35,7 @@ const CampaignLaunchSuccess = () => {
          xmlns="http://www.w3.org/2000/svg"
          className="w-8 h-8 text-white"
          fill="none"
-         viewBox="0 0 24 24"
+         viewBox="0 0 23 23"
          stroke="currentColor"
          strokeWidth={3}
           >
